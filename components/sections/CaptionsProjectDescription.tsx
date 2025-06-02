a98a1ba54a728883bfc105dc9eb69b9ffc68f551
Conrad Jamir <conradjamir@gmail.com>
2025-06-02 11:41:09 -0700

    Update project description and styles

--- components/sections/CaptionsProjectDescription.tsx
+++ components/sections/CaptionsProjectDescription.tsx
@@ -5,7 +5,7 @@ interface CaptionsProjectDescriptionProps {
 }
 export default function CaptionsProjectDescription({ role, technology, notes }: CaptionsProjectDescriptionProps) {
   return (
-    <div className="flex justify-center py-4 lg:max-w-[75%] m-auto text-sm">
+    <div className="flex justify-center lg:max-w-[75%] m-auto text-sm mobilePortrait:max-h-[200px] mobileLandscape:max-h-[100px] overflow-y-auto">
         <ul>
             <li className="text-white font-bold tracking-[3px] uppercase leading-6 pb-2 hyphens-none">Role: <span className="font-normal">{role}</span></li>
             <li className="text-white font-bold tracking-[3px] uppercase leading-6 pb-2 hyphens-none">Technology: <span className="font-normal">{technology}</span></li>
