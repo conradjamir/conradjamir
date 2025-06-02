a352708fdfe4a04a6205492e75a867cebde12375
Conrad Jamir <conradjamir@gmail.com>
2025-06-02 12:55:19 -0700

    Seans Surprise Birthday Landingn Page.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/surprise-birthday',
+        destination: '/surprise-birthday/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
